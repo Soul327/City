d1edc19d6ec515fa69d44fc2a188cc8b3a60ae67
Soul327 <soulsilver27@hotmail.com>
2018-07-17 22:41:24 -0500

    test

--- OPen/src/Main/Launcher.java
+++ OPen/src/Main/Launcher.java
@@ -3,6 +3,7 @@ package Main;
 public class Launcher {
 	public static void main(String[] args){
 		Game game = new Game("OPen",1280,720);
-		game.start();
+		game.start();//test
+		System.out.println("test");
 	}
 }
