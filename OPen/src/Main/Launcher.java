3b6108263feb3037ae5889e97ee98a1d39cbff33
Soul327 <soulsilver27@hotmail.com>
2018-07-18 00:44:59 -0500

    testasdfasdf

--- OPen/src/Main/Launcher.java
+++ OPen/src/Main/Launcher.java
@@ -3,7 +3,6 @@ package Main;
 public class Launcher {
 	public static void main(String[] args){
 		Game game = new Game("OPen",1280,720);
-		game.start();//test
-		System.out.println("tes;kjt");
+		game.start();
 	}
 }
